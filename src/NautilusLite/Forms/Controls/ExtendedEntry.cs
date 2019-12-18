796ce23f6f9fd5e4a181474ced6d43e3a6e51199
Shah Z. S <shahzshafie@outlook.com>
2019-12-18 20:16:20 +0800

    update entry abstraction layer

--- src/NautilusLite/Forms/Controls/ExtendedEntry.cs
+++ src/NautilusLite/Forms/Controls/ExtendedEntry.cs
@@ -1,13 +1,56 @@
-﻿using System;
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 
 namespace NautilusLite.Forms.Controls
 {
 	public class ExtendedEntry : Entry
 	{
-		public ExtendedEntry()
+		#region BorderWidthProperty
+		public static readonly BindableProperty BorderWidthProperty =
+		BindableProperty.Create("float", typeof(float), typeof(ExtendedEntry), 1.0f);
+
+		public float BorderWidth
 		{
-			
+			get => (float)GetValue(BorderWidthProperty);
+			set => SetValue(BorderWidthProperty, value);
 		}
+		#endregion
+
+		#region BorderColorProperty
+		public static readonly BindableProperty BorderColorProperty =
+		BindableProperty.Create(nameof(Color), typeof(Color), typeof(ExtendedEntry), Color.White);
+
+		public Color BorderColor
+		{
+			get => (Color)GetValue(BorderColorProperty);
+			set => SetValue(BorderColorProperty, value);
+		}
+		#endregion
+
+		#region BorderStyleProperty
+		public static readonly BindableProperty BorderStyleProperty =
+		BindableProperty.Create(nameof(BorderStyle), typeof(BorderStyle), typeof(ExtendedEntry), BorderStyle.Default);
+
+		public BorderStyle BorderStyle
+		{
+			get => (BorderStyle)GetValue(BorderStyleProperty);
+			set => SetValue(BorderStyleProperty, value);
+		}
+		#endregion
+	}
+
+	public enum BorderStyle
+	{
+		/// <summary>
+		/// Default border for all platforms
+		/// </summary>
+		Default,
+		/// <summary>
+		/// No border
+		/// </summary>
+		None,
+		/// <summary>
+		/// Underline border only
+		/// </summary>
+		Underline,
 	}
 }
