5c78725442bb5241b5a5190eece018fd06b2ce2f
Shah Z. S <shahzshafie@outlook.com>
2019-12-19 15:58:21 +0800

    update

--- src/NautilusLite/Infrastructure/DebugOutput.cs
+++ src/NautilusLite/Infrastructure/DebugOutput.cs
@@ -1,10 +1,74 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
 namespace NautilusLite.Infrastructure
 {
-	public class DebugOutput
+	public static class DebugOutput
 	{
-		public DebugOutput()
+		public static string GetClassAndMemberName(Type type, [CallerMemberName] string callerName = null)
 		{
+			return $"[{type.Name}.{callerName}]";
+		}
+
+		public static string GetTypeString(Type type, [CallerMemberName] string callerName = null)
+		{
+			return $"[{type.Name}.{callerName}]";
+		}
+
+		[Conditional("DEBUG")]
+		public static void Write(Type type, string message = "", [CallerMemberName] string callerName = null)
+		{
+			var outputMessage = string.Empty;
+
+			if (string.IsNullOrWhiteSpace(message))
+			{
+				outputMessage = $"[{type.Name}.{callerName}]";
+			}
+			else
+			{
+				outputMessage = $"[{type.Name}.{callerName}] : {message}";
+			}
+
+			Debug.WriteLine(outputMessage);
+		}
+
+		[Conditional("DEBUG")]
+		public static void Write(Type type, string message, Exception exceptionThrown, [CallerMemberName] string callerName = null)
+		{
+			var outputMessage = exceptionThrown != null ?
+				$"[{type.Name}.{callerName}] : {exceptionThrown.GetType().ToString()} : {message}" :
+				$"[{type.Name}.{callerName}] : {message}";
+
+			Debug.WriteLine(outputMessage);
+		}
+
+		[Conditional("DEBUG")]
+		public static void Write(string message, [CallerMemberName] string callerName = null)
+		{
+			Write(message, null, callerName);
+		}
+
+		[Conditional("DEBUG")]
+		public static void Write(Exception exceptionThrown, [CallerMemberName] string callerName = null)
+		{
+			Write(exceptionThrown.Message, exceptionThrown, callerName);
+		}
+
+		[Conditional("DEBUG")]
+		public static void Write(string message, Exception exceptionThrown, [CallerMemberName] string callerName = null)
+		{
+			var outputMessage = exceptionThrown != null ?
+				$"[{callerName}] : {exceptionThrown.GetType().ToString()} : {message}" :
+				$"[{callerName}] : {message}";
+
+			Debug.WriteLine(outputMessage);
+		}
+
+		[Conditional("DEBUG")]
+		public static void WriteForStaticMethod(string classAndMemberName, string message)
+		{
+			Debug.WriteLine($"{classAndMemberName} : {message}");
 		}
 	}
 }
