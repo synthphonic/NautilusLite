0ac204f362585470326cfb72e0b4498db62c588d
Shah Z. S <shahzshafie@outlook.com>
2019-12-23 17:11:35 +0800

    add xamarin essentials package, add LocalStorage

---
+++ src/NautilusLite/Infrastructure/IO/LocalStorage.cs
@@ -0,0 +1,26 @@
+﻿namespace NautilusLite.Infrastructure.IO
+{
+	public sealed class LocalStorage
+	{
+		#region Singleton pattern
+		private static readonly LocalStorage _instance = new LocalStorage();
+
+		private LocalStorage()
+		{
+			SetPath();
+		}
+
+		public static LocalStorage Instance
+		{
+			get { return _instance; }
+		}
+		#endregion
+
+		private void SetPath()
+		{
+			RootPath = Xamarin.Essentials.FileSystem.AppDataDirectory;
+		}
+
+		public string RootPath { get; private set; }
+	}
+}
