836678359bb0bf0edb4c360c817fa733352f479a
Shah Z. S <shahzshafie@outlook.com>
2019-12-25 20:34:23 +0800

    add FontInfoHelper class to show all installed fonts in the device - THIS IS FOR DEBUGGING ONLY

---
+++ src/NautilusLite.iOS/Services/FontInfoHelper.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * Reference:
+ * https://stackoverflow.com/questions/48188187/fontawesome-pro-and-xamarin-ios-only-one-font-can-be-active
+ * https://forums.xamarin.com/discussion/94921/font-awesome-problem
+ *
+ */
+using System.Linq;
+using NautilusLite.Infrastructure;
+using UIKit;
+
+namespace NautilusLite.iOS.Services
+{
+	public static class FontInfoHelper
+	{
+		/// <summary>
+		/// Shows all installed fonts to the debut output window
+		/// <para/>
+		/// NOTE: This class is to be used for debugging ONLY
+		/// </summary>
+		public static void OutputInstalledFonts()
+		{
+			foreach (var familyNames in UIFont.FamilyNames.OrderBy(c => c).ToList())
+			{
+				DebugOutput.Write(" * " + familyNames);
+				foreach (var familyName in UIFont.FontNamesForFamilyName(familyNames).OrderBy(c => c).ToList())
+				{
+					DebugOutput.Write(" *-- " + familyName);
+				}
+			}
+
+		}
+	}
+}
