ae4f9751a3088ae1447e9d263b97bc0b9ce39502
Shah Z. S <shahzshafie@outlook.com>
2019-12-26 21:09:24 +0800

    date only converter

---
+++ src/Samples/Todo/Todo/Converters/DateOnlyConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace Todo.Converters
+{
+	public class DateOnlyConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			var dateTime = (DateTime)value;
+
+			return dateTime.ToString("dd/MM/yyyy");
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
