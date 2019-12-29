7c4654d9512390d804a486fff8c60ce32cc71644
Shah Z. S <shahzshafie@outlook.com>
2019-12-29 13:18:54 +0800

    make model bindable properties

--- src/Samples/Todo/Todo/Models/TodoItem.cs
+++ src/Samples/Todo/Todo/Models/TodoItem.cs
@@ -5,31 +5,54 @@ namespace Todo.Models
 {
 	public class TodoItem : ViewModelBase
 	{
+		private Guid _id;
+		private DateTime _due;
 		private string _name;
+		private string _description;
+		private string _shortDescription;
 		private bool _completed;
 		private bool _isFavorite;
 
 		public TodoItem()
 		{
-			_isFavorite = true;
+			_isFavorite = false;
 		}
 
-		public Guid Id { get; set; }
+		public Guid Id
+		{
+			get => _id;
+			set => Set(ref _id, value);
+		}
+
 		public string Name
 		{
 			get => _name;
 			set => Set(ref _name, value);
 		}
 
-		public string Description { get; set; }
-		public string ShortDescription { get; set; }
+		public string Description
+		{
+			get => _description;
+			set => Set(ref _description, value);
+		}
+
+		public string ShortDescription
+		{
+			get => _shortDescription;
+			set => Set(ref _shortDescription, value);
+		}
+
 		public bool IsFavorite
 		{
 			get => _isFavorite;
 			set => Set(ref _isFavorite, value);
 		}
 
-		public DateTime Due { get; set; }
+		public DateTime Due
+		{
+			get => _due;
+			set => Set(ref _due, value);
+		}
 
 		public bool Completed
 		{
