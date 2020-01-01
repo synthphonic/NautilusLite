2ddae1e2491ad8a7f61494d63db61f728da95beb
Shah Z. S <shahzshafie@outlook.com>
2020-01-01 22:16:15 +0800

    implement NautilusReturnResult class

---
+++ src/NautilusLite/Core/NautilusReturnResult.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace NautilusLite.Core
+{
+	public class NautilusReturnResult
+	{
+		public NautilusReturnResult(bool isSuccessful)
+		{
+			IsSuccessful = isSuccessful;
+		}
+
+		internal void SetException(Exception ex)
+		{
+			Exception = ex;
+		}
+
+		internal void SetMessage(string message)
+		{
+			Message = message;
+		}
+
+		public bool IsSuccessful { get; private set; }
+		public Exception Exception { get; private set; }
+		public string Message { get; private set; }
+
+		public static NautilusReturnResult ReturnSuccess()
+		{
+			return new NautilusReturnResult(true);
+		}
+
+		internal static NautilusReturnResult ReturnException(Exception ex)
+		{
+			var result = new NautilusReturnResult(false);
+			result.SetException(ex);
+
+			return result;
+		}
+
+		internal static NautilusReturnResult ReturnErrorMessage(string message)
+		{
+			var result = new NautilusReturnResult(false);
+			result.SetMessage(message);
+
+			return result;
+		}
+	}
+}
